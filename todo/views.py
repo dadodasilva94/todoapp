0129a6f92ffda8991b69c587c2dd38c708557f4b
Paulo Eduardo <dado@pdsolucoes.com.br>
2021-06-20 17:54:29 -0300

    feat: Creating the view and url path for the API

--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import TodoSerializer
+from .models import Todo
+
 
 # Create your views here.
+
+#Provide the implementation for CRUD op by default. This code specifies the serializer class and queryset
+class TodoView(viewsets.ModelViewSet):
+    serializer_class = TodoSerializer
+    queryset = Todo.objects.all()
